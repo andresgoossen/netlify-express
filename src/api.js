c311169e9eade2ababfd768843ee978f5022fdc2
andresgoossen <44237404+andresgoossen@users.noreply.github.com>
2022-07-25 18:52:59 -0300

    update api

--- src/api.js
+++ src/api.js
@@ -3,14 +3,20 @@ const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
-
+let a = "holas"
 router.get("/", (req, res) => {
   res.json({
-    hello: "hi!"
+    hello: a
+  });
+});
+router.get("/h", (req, res) => {
+  a ="andriy"
+  res.json({
+    hello: a
   });
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/apis`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
